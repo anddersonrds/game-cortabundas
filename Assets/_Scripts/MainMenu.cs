fc3ef25196a3d20b71bf818cac92503ce78d1966
AntonioCarlosTavares <33660339+AntonioCarlosTavares@users.noreply.github.com>
2018-08-24 17:44:32 -0300

    Alterações no projeto
    Várias coisas

---
+++ Assets/_Scripts/MainMenu.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject mainMenu,optionsMenu;
+
+
+
+    public void Play()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);        
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+
+        Debug.Log("Saiu");
+    }
+
+    public void OptionsMenu()
+    {
+        mainMenu.SetActive(false);
+
+        optionsMenu.SetActive(true);
+    }
+
+    public void BackMenu()
+    {
+        optionsMenu.SetActive(false);
+
+        mainMenu.SetActive(true);
+    }
+
+
+
+}
