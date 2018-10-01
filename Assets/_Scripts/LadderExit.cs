af06c1179204a17f5d4c467f49058b02517d07ca
AntonioCarlosTavares <33660339+AntonioCarlosTavares@users.noreply.github.com>
2018-10-01 16:12:57 -0300

    Escada
    Corrigido bug que fazia o player travar na escada.

---
+++ Assets/_Scripts/LadderExit.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LadderExit : MonoBehaviour
+{
+    public GameObject player;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject == player)
+        {
+            player.gameObject.GetComponent<Player>().enabled = true;
+        }
+    }
+
+}
