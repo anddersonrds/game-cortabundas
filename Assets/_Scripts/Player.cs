88d866d6767bc95d97980d4799da967811ee60bb
Anderson Rodrigues <anddersonrds@gmail.com>
2018-10-10 05:00:27 -0300

    add run and stamina

--- Assets/_Scripts/Player.cs
+++ Assets/_Scripts/Player.cs
@@ -1,30 +1,65 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
-    public float speed = 10.0F;  
+    public float speed = 10.0F;
 
-     void Start()
+    public Slider staminaSlider;
+    public int maxStamina;
+    private int staminaFall;
+    public int staminaFallMult;
+    private int staminaRegen;
+    public int staminaRegenMult;
+
+    void Start()
     {
-        Cursor.lockState = CursorLockMode.Locked;        
+        Cursor.lockState = CursorLockMode.Locked;
+
+        staminaSlider.maxValue = maxStamina;
+        staminaSlider.value = maxStamina;
+
+        staminaFall = 1;
+        staminaRegen = 1;
     }
 
     void Update()
     {
       
-            float translation = Input.GetAxis("Vertical") * speed;
-            float straffe = Input.GetAxis("Horizontal") * speed;
-            translation *= Time.deltaTime;
-            straffe *= Time.deltaTime;
+        float translation = Input.GetAxis("Vertical") * speed;
+        float straffe = Input.GetAxis("Horizontal") * speed;
+        translation *= Time.deltaTime;
+        straffe *= Time.deltaTime;
 
-            transform.Translate(straffe, 0, translation);
+        transform.Translate(straffe, 0, translation);
 
-            if (Input.GetKeyDown("escape"))
-            {
-                Cursor.lockState = CursorLockMode.None;
-            }
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            staminaSlider.value -= Time.deltaTime / staminaFall * staminaFallMult;
+            speed = 12.0F;
+        }
+        else
+        {
+            staminaSlider.value += Time.deltaTime / staminaRegen * staminaRegenMult;
+            speed = 6.0F;
+        }
+
+        if (staminaSlider.value >= maxStamina)
+        {
+            staminaSlider.value = maxStamina;
+        }
+        else if (staminaSlider.value <= 0)
+        {
+            staminaSlider.value = 0;
+            speed = 3.0F;
+        }
+
+        if (Input.GetKeyDown("escape"))
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
