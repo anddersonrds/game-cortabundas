af51699ddd364d182bd336dc93355e7ced04ce67
AntonioCarlosTavares <33660339+AntonioCarlosTavares@users.noreply.github.com>
2018-11-23 23:36:48 -0300

    Revert "Ajuste BoxCollider"
    This reverts commit c028ba7922ede7681c71d3651328f01823179415.

--- Assets/_Scripts/Player.cs
+++ Assets/_Scripts/Player.cs
@@ -165,7 +165,7 @@ public class Player : MonoBehaviour
     {
         if (other.gameObject.name == "Cena1Checkpoint")
         {
-            GameObject ai = GameObject.Find("EnemyNPC");
+            GameObject ai = GameObject.Find("EnemyNPCCena1");
             GameObject area = GameObject.Find("StayArea");
 
             NpcAi aiScript = ai.GetComponent<NpcAi>();
