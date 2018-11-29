064e134d5779fe81903b3ecc859206f1009f7bf9
Daniel Pinto Coutinho <daniel.pintocoutinho@gmail.com>
2018-11-29 14:04:56 -0300

    Adicionando shader com efeito de sombra e trigger

---
+++ Assets/_Scripts/ShadowEffect.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShadowEffect : MonoBehaviour
+{
+    private Material material;
+    float timer;
+    bool triggered;
+
+    public float hitPeriod = 1.0f;
+    // Creates a private material used to the effect
+    void Awake()
+    {
+        material = new Material(Shader.Find("ShadowEffect"));
+        triggered = false;
+        timer = 0.0f;
+    }
+
+    private void Update()
+    {
+        if (triggered)
+        {
+            timer += Time.deltaTime;
+            if (timer > hitPeriod)
+                triggered = false;
+        }
+    }
+
+    // Postprocess the image
+    void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        if (triggered)
+            Graphics.Blit(source, destination, material);
+        else
+            Graphics.Blit(source, destination);
+    }
+
+    public void StartShader()
+    {
+        timer = 0.0f;
+        triggered = true;
+    }
+}
