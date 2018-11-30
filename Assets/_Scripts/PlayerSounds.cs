6edce260a390fdb7b58b4cc962dff0a32cf27c42
Anderson Rodrigues <anddersonrds@gmail.com>
2018-11-29 22:29:14 -0300

    add ScriptSound

---
+++ Assets/_Scripts/PlayerSounds.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSounds : MonoBehaviour {
+
+    public AudioSource walkingSound, runningSound;
+
+	void Start () {
+		
+	}
+	
+	void Update () 
+    {
+        MovimentSound();
+	}
+
+    private void MovimentSound()
+    {
+        if(Input.GetKey(KeyCode.LeftShift))
+        {
+            runningSound.Play();
+        }
+        else{
+            walkingSound.Play();
+        }
+    }
+}
