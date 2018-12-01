dcf5c1473c5db5a402d19f96b97f273958c52fa3
AntonioCarlosTavares <33660339+AntonioCarlosTavares@users.noreply.github.com>
2018-12-01 03:58:18 -0300

    Escadas do demonio
    Escada agora é a prova de cair pra fora da fase, testem até o olho arder.

--- Assets/_Scripts/LadderClimb.cs
+++ Assets/_Scripts/LadderClimb.cs
@@ -7,17 +7,25 @@ public class LadderClimb : MonoBehaviour
     public GameObject player;
     public bool canClimb = false;
     public float speed = 3;
-   
-    
+    private Collider coll;
+
+
+    private void Start()
+    {
+        coll = GetComponent<Collider>();
+        coll.isTrigger = true;
+    }
+
+
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject == player)
+        if (other.attachedRigidbody)
         {
             other.attachedRigidbody.useGravity = false;
             other.attachedRigidbody.isKinematic = true;
             Debug.Log(other.attachedRigidbody.useGravity);
-            player.gameObject.GetComponent<Player>().enabled = false;            
+            player.gameObject.GetComponent<Player>().enabled = false;         
             canClimb = true;              
         }
     }
@@ -36,23 +44,16 @@ public class LadderClimb : MonoBehaviour
     void Update ()
     {
         if (canClimb)
-        {           
-
+        {
             if (Input.GetKey(KeyCode.W))
             {               
-                player.transform.Translate ( new Vector3 (0, 1, 0) * Time.deltaTime * speed);               
+                player.transform.Translate (Vector3.up * speed * Time.deltaTime);               
             }
 
             if (Input.GetKey(KeyCode.S))
             {
                 player.transform.Translate (new Vector3 (0,-1, 0) * Time.deltaTime * speed);
             }
-        }
-
-        else
-        {
-            player.GetComponent<Collider>().attachedRigidbody.useGravity = true;
-        }
-		
+        }		
 	}
 }
--- Assets/_Scripts/LadderExit.cs
+++ Assets/_Scripts/LadderExit.cs
@@ -11,6 +11,7 @@ public class LadderExit : MonoBehaviour
         if(other.gameObject == player)
         {
             player.gameObject.GetComponent<Player>().enabled = true;
+            player.gameObject.GetComponent<Rigidbody>().isKinematic = false;
         }
     }
 
