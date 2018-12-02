e81fe02b09459311fdf6891e97b82f18b466d860
AntonioCarlosTavares <33660339+AntonioCarlosTavares@users.noreply.github.com>
2018-12-02 00:30:49 -0300

    Cinemachine

---
+++ Assets/CinemachineEvent.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CinemachineEvent : MonoBehaviour
+{
+    public GameObject CutScenePlayer;
+    
+    public void DeletePlayer()
+    {
+        CutScenePlayer.SetActive(false);
+    }
+	
+}
