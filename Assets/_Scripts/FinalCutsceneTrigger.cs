a78ca121b4596bbc228bf1ba2b2c8cbc08bf9f2f
Daniel Pinto Coutinho <daniel.pintocoutinho@gmail.com>
2018-12-03 04:33:04 -0300

    Adicionando cutscene final

---
+++ Assets/_Scripts/FinalCutsceneTrigger.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinalCutsceneTrigger : MonoBehaviour {
+
+    public GameObject cutSceneTimeLine;
+    public GameObject gameOverTrigger;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        cutSceneTimeLine.SetActive(true);
+        gameOverTrigger.SetActive(true);
+    }
+}
---
+++ Assets/_Scripts/GameOverTrigger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverTrigger : MonoBehaviour {
+
+    public GameMaster gm;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        gm.GameOver();
+    }
+}
