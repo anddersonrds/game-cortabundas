065e6e0520ba4daf6cae58f2e436c1d8c41ff27c
Daniel Pinto Coutinho <daniel.pintocoutinho@gmail.com>
2018-12-05 21:52:47 -0300

    Ajeitando trigger final

---
+++ Assets/_Scripts/SoundOffTrigger.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundOffTrigger : MonoBehaviour {
+
+    public GameObject musicalBox;
+    public GameObject finalTrigger;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        finalTrigger.SetActive(true);
+        musicalBox.GetComponent<AudioSource>().Stop();
+    }
+}
